482028be9540da332236f52e2daf52364b456950
Michael Gattozzi <mgattozzi@gmail.com>
2020-02-26 23:11:01 -0500

    Simplify router logic and add all HTTP verbs to it
    During the prototyping phase we implemented a `Service` by hand in order
    to get the hyper server. However, this was unnecessary and the type and
    it's impls could be removed leaving just the `Router` type to which we
    have also added the last functions needed in order to add routes for
    each of the HTTP verb types defined in IETF RFC 7231.

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,10 @@ use std::{
   future::Future,
   sync::Arc,
 };
+
 #[derive(Debug)]
 pub struct Router<F> {
-  post_routes: HashMap<Cow<'static, str>, Arc<F>>,
+  routes: HashMap<Method, HashMap<Cow<'static, str>, Arc<F>>>,
 }
 
 impl<F, G> Router<F>
@@ -27,8 +28,30 @@ where
 {
   pub fn new() -> Self {
     Self {
-      post_routes: HashMap::new(),
+      routes: HashMap::new(),
     }
+  }
+
+  pub fn get(mut self, path: impl Into<Cow<'static, str>>, handler: F) -> Self {
+    let routes = self.routes.entry(Method::GET).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
+  }
+
+  pub fn put(mut self, path: impl Into<Cow<'static, str>>, handler: F) -> Self {
+    let routes = self.routes.entry(Method::PUT).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
+  }
+
+  pub fn delete(
+    mut self,
+    path: impl Into<Cow<'static, str>>,
+    handler: F,
+  ) -> Self {
+    let routes = self.routes.entry(Method::DELETE).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
   }
 
   pub fn post(
@@ -36,25 +59,65 @@ where
     path: impl Into<Cow<'static, str>>,
     handler: F,
   ) -> Self {
-    self.post_routes.insert(path.into(), Arc::new(handler));
+    let routes = self.routes.entry(Method::POST).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
+  }
+
+  pub fn trace(
+    mut self,
+    path: impl Into<Cow<'static, str>>,
+    handler: F,
+  ) -> Self {
+    let routes = self.routes.entry(Method::TRACE).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
+  }
+
+  pub fn patch(
+    mut self,
+    path: impl Into<Cow<'static, str>>,
+    handler: F,
+  ) -> Self {
+    let routes = self.routes.entry(Method::PATCH).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
+  }
+
+  pub fn options(
+    mut self,
+    path: impl Into<Cow<'static, str>>,
+    handler: F,
+  ) -> Self {
+    let routes = self.routes.entry(Method::OPTIONS).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
+    self
+  }
+
+  pub fn head(
+    mut self,
+    path: impl Into<Cow<'static, str>>,
+    handler: F,
+  ) -> Self {
+    let routes = self.routes.entry(Method::HEAD).or_insert(HashMap::new());
+    routes.insert(path.into(), Arc::new(handler));
     self
   }
 
   fn find_handler(&self, req: &Request<Body>) -> Option<Arc<F>> {
-    match (req.method(), req.uri().path()) {
-      (&Method::POST, path) => self.post_routes.get(path).map(|h| h.clone()),
-      _ => todo!(),
-    }
+    self
+      .routes
+      .get(req.method())
+      .and_then(|r| r.get(req.uri().path()).map(|h| h.clone()))
   }
 
   pub async fn serve(self) -> Result<(), hyper::Error> {
-    let service = Arc::new(RouterService::new(self));
+    let router = Arc::new(self);
     let service_fn = make_service_fn(|_| {
-      let service = service.clone();
+      let router = router.clone();
       async move {
         Ok::<_, hyper::Error>(service_fn(move |req| {
-          let value = service.router.find_handler(&req).unwrap()(req);
-
+          let value = router.find_handler(&req).unwrap()(req);
           async { Ok::<_, hyper::Error>(value.await) }
         }))
       }
@@ -62,20 +125,5 @@ where
     Server::bind(&([0, 0, 0, 0], 8080).into())
       .serve(service_fn)
       .await
-  }
-}
-
-#[derive(Debug)]
-pub struct RouterService<F> {
-  pub router: Router<F>,
-}
-
-impl<F, G> RouterService<F>
-where
-  F: Send + Sync + 'static + Fn(Request<Body>) -> G,
-  G: Future<Output = Response<Body>> + Send + Sync + 'static,
-{
-  fn new(router: Router<F>) -> Self {
-    Self { router }
   }
 }
